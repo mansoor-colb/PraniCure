b5518f118199e12dd48d4a7b9f89292f5a4b8a39
tejassp03 <tejassp03@gmail.com>
2023-10-15 00:05:06 +0530

    major ui fixes

--- ngoapp/public/assets/js/navbaractivate.js
+++ ngoapp/public/assets/js/navbaractivate.js
@@ -1,33 +1,23 @@
-window.addEventListener('DOMContentLoaded', function() {
-  fetch("navbar.html")
-    .then((response) => response.text())
-    .then((content) => {
-      document.getElementById("navbarContainer").innerHTML = content;
-      activate();
-    });
-});
 
-function activate() {
+document.addEventListener("DOMContentLoaded", function () {
+
   var parentDocument = window.parent.document;
   var page = window.location.href.split('/').pop();
 
-  if (page === "user-home.html") {
+  if (page === "indexngo.html") {
     console.log("user home clicked!");
-    const userHomeLink = parentDocument.getElementById("homelink");
+    let userHomeLink = parentDocument.getElementById("homelink");
     if (userHomeLink) {
       userHomeLink.classList.add("active");
     }
   }
-  else if(page==="indexuser.html"){
-    const userHomeLink = parentDocument.getElementById("notiflink");
-    userHomeLink.classList.add("active");
-  }
-  else if(page==="order-list.html"){
-      const userHomeLink = parentDocument.getElementById("messagelink");
+  else if(page==="ngo-order-list.html"){
+      let userHomeLink = parentDocument.getElementById("messagelink");
       userHomeLink.classList.add("active");
   }
+
   // else if(page==="profile.html"){
   //     const userHomeLink =parentDocument.getElementById("proflink");
   //     userHomeLink.classList.add("active");
   // }
-}
+});
--- userapp/public/assets/js/navbaractivate.js
+++ userapp/public/assets/js/navbaractivate.js
@@ -1,33 +1,30 @@
-window.addEventListener('DOMContentLoaded', function() {
-  fetch("navbar.html")
-    .then((response) => response.text())
-    .then((content) => {
-      document.getElementById("navbarContainer").innerHTML = content;
-      activate();
-    });
-});
 
-function activate() {
+document.addEventListener("DOMContentLoaded", function () {
+
   var parentDocument = window.parent.document;
   var page = window.location.href.split('/').pop();
 
   if (page === "user-home.html") {
     console.log("user home clicked!");
-    const userHomeLink = parentDocument.getElementById("homelink");
+    let userHomeLink = parentDocument.getElementById("homelink");
     if (userHomeLink) {
       userHomeLink.classList.add("active");
     }
   }
   else if(page==="indexuser.html"){
-    const userHomeLink = parentDocument.getElementById("notiflink");
-    userHomeLink.classList.add("active");
+    console.log("index home clicked!");
+    let userHomeLink = parentDocument.getElementById("notiflink");
+    if (userHomeLink) {
+      userHomeLink.classList.add("active");
+    }
   }
   else if(page==="order-list.html"){
-      const userHomeLink = parentDocument.getElementById("messagelink");
+      let userHomeLink = parentDocument.getElementById("messagelink");
       userHomeLink.classList.add("active");
   }
+
   // else if(page==="profile.html"){
   //     const userHomeLink =parentDocument.getElementById("proflink");
   //     userHomeLink.classList.add("active");
   // }
-}
+});
